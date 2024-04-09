d74c649d80318e99b18722aa17cfa1e2fafbaa90
DavidGabrielBJorge <davidgbjorge@gmail.com>
2024-04-09 11:11:02 -0300

    add: criando a estrutura base do projeto comecando com o React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import "./index.css";
+import Movie from './pages/Movie.jsx';
+import Search from './pages/Search.jsx';
+import Home from './pages/Home.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App></App>}>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='movie/:id' element={<Movie></Movie>}></Route>
+          <Route path='search' element={<Search></Search>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )

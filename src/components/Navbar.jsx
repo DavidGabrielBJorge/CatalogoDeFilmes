1fff05acfb5fc3c1c9db6f64fc000bd5ad04a153
DavidGabrielBJorge <davidgbjorge@gmail.com>
2024-04-15 11:01:05 -0300

    add: terminando de customizar o css do movies-card

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
-import { Link } from "react-router-dom"
+
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
 import "./Navbar.css";
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
   return (
     <nav id="navbar">
     <h2>
       <Link to="/"> <BiCameraMovie/> Movies Lib</Link>
     </h2>
     <form>
-        <input type="text" placeholder="Search a movie..."/>
+        <input type="text" placeholder="Search a movie..." onChange={(e) => setSearch(e.target.value)} value={search}/>
         <button type="submit"> <BiSearchAlt2/></button>
     </form>
   </nav>
